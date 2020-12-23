4a5c33431654e286c406b26d1a8101f41797ca66
treasureBear94 <qvo7896@gmail.com>
2020-12-23 22:59:06 +0900

    :sparkles: add Appbar icon

--- README.md
+++ README.md
@@ -2,15 +2,11 @@
 
 A new Flutter project.
 
-## Getting Started
+[유튜브 코딩셰프님의 강의](https://youtu.be/ze0t5gWKBvE)
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+## AppBar icon button
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- leading: 아이콘 버튼이나 간단한 위젯을 왼쪽에 배치 
+- actions: 복수의 아이콘 버튼 등을 오른쪽에 배치
+- onPressed: 터치 시, 이벤트 정의
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appbar icon menu'),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.menu), 
+          onPressed: () { 
+            print('menu button is clicked');
+           },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart), 
+            onPressed: () { 
+              print('Shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: () { 
+              print('search button is clicked');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
